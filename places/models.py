50d89141ae4e31930f7b9845450dddad651964a1
sammels <sasagete@gmail.com>
2023-12-24 20:19:01 +0300

    Add PlaceImage models

--- places/models.py
+++ places/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField(blank=True)
@@ -11,3 +12,10 @@ class Place(models.Model):
     def __str__(self):
         return self.title
 
+
+class PlaceImage(models.Model):
+    title = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
