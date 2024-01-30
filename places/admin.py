9edc8fb6d1026b2b0e4cc240202e50fa81d13830
sammels <sasagete@gmail.com>
2024-01-30 14:30:11 +0300

    feature: add preview

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 from .models import Place, Image
 
@@ -6,10 +8,18 @@ from .models import Place, Image
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    readonly_fields = ['image_preview']
+
+    def image_preview(self, obj):
+        return format_html('<img src="{}" style="max-height: 200px;"/>',
+                           mark_safe(obj.image.url))
 
 
 @admin.register(Place)
 class ImageAdmin(admin.ModelAdmin):
+
     inlines = [
         ImageInline,
     ]
+
+
