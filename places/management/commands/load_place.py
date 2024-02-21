dd712d33625054548391d39f3c8d3294cd555441
sammels <sasagete@gmail.com>
2024-02-21 16:06:17 +0300

    fix: rename variable

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         response = requests.get(url)
         response.raise_for_status()
         place = response.json()
-        new_place, _ = Place.objects.get_or_create(
+        payload, _ = Place.objects.get_or_create(
             title=place['title'],
             defaults={
                 'short_description': place['description_short'],
@@ -35,4 +35,4 @@ class Command(BaseCommand):
             response = requests.get(image_url)
             response.raise_for_status()
             image_content = ContentFile(response.content, name=os.path.split(image_url)[1])
-            Image.objects.create(place=new_place, image=image_content)
+            Image.objects.create(place=payload, image=image_content)
